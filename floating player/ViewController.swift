1319a50a2b1e2ff9e834b4a56b8fe663dd7e49cf
jitae <jitaekim93@gmail.com>
2018-05-06 15:32:55 -0700

    Initial Commit

---
+++ floating player/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  floating player
+//
+//  Created by Jitae Kim on 5/6/18.
+//  Copyright © 2018 Jitae Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
